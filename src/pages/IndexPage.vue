dabef18d8b18d52939e44b3bca654600c58c6d5f
zpzhou <himoker@gmail.com>
2023-03-13 15:10:26 +0800

    update

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,67 +1,67 @@
 <template>
-  <q-page style="min-height: 0;">
+    <q-page style="min-height: 0;">
 
-    <div class="q-pa-md column no-wrap">
+        <div class="q-pa-md column no-wrap">
 
-      <!--聊天内容-->
-      <q-scroll-area style="height: calc(100vh - 148px);" ref="scrollAreaRef" @scroll="autoScroll">
-        <div class="row justify-center">
-          <div style="width: 100%; max-width: 800px;">
-            <q-chat-message
-              style="white-space: pre-wrap;"
-              v-for="(msg, index) in DisplayMessages" :key="index"
-              :name='msg.sent ? "Me": "AI"'
-              :text=[msg.text]
-              :avatar='msg.sent ? meImg: aiImg'
-              :sent=msg.sent
-              text-html
-            />
+            <!--聊天内容-->
+            <q-scroll-area style="height: calc(100vh - 148px);" ref="scrollAreaRef" @scroll="autoScroll">
+                <div class="row justify-center">
+                    <div style="width: 100%; max-width: 800px;">
+                        <q-chat-message
+                            style="white-space: pre-wrap;"
+                            v-for="(msg, index) in DisplayMessages" :key="index"
+                            :name='msg.sent ? "Me": "AI"'
+                            :text=[msg.text]
+                            :avatar='msg.sent ? meImg: aiImg'
+                            :sent=msg.sent
+                            text-html
+                        />
 
-            <q-chat-message
-              v-if="Loading"
-              name="AI"
-              :avatar="aiImg"
-            >
-              <q-spinner-dots size="2rem"/>
-            </q-chat-message>
+                        <q-chat-message
+                            v-if="Loading"
+                            name="AI"
+                            :avatar="aiImg"
+                        >
+                            <q-spinner-dots size="2rem"/>
+                        </q-chat-message>
 
-            <q-chat-message
-              style="white-space: pre-wrap;"
-              v-if="Waiting"
-              name="AI"
-              :avatar="aiImg"
-              :text=[waitText]
-            />
+                        <q-chat-message
+                            style="white-space: pre-wrap;"
+                            v-if="Waiting"
+                            name="AI"
+                            :avatar="aiImg"
+                            :text=[waitText]
+                        />
 
-          </div>
+                    </div>
+                </div>
+            </q-scroll-area>
+
+            <!--输入框-->
+            <div class="row justify-center">
+                <div class="row justify-center" style="width: 100%; max-width: 800px">
+                    <q-input
+                        square
+                        class="col-12 col-md-10"
+                        :disable="Loading"
+                        @keydown.enter="handleEnter"
+                        filled autogrow bg-color="grey"
+                        v-model="InputText"
+                        label="向OpenAI提问"/>
+
+                    <q-btn
+                        square
+                        @click="StreamChat"
+                        unelevated
+                        class="col-12 col-md-2"
+                        color="secondary"
+                    >
+                        <div>发送(Ctrl+Enter)</div>
+                    </q-btn>
+                </div>
+            </div>
         </div>
-      </q-scroll-area>
-
-      <!--输入框-->
-      <div class="row justify-center">
-        <div class="row justify-center" style="width: 100%; max-width: 800px">
-          <q-input
-            square
-            class="col-12 col-md-10"
-            :disable="Loading"
-            @keydown.enter="handleEnter"
-            filled autogrow bg-color="grey"
-            v-model="InputText"
-            label="向OpenAI提问"/>
-
-          <q-btn
-            square
-            @click="StreamChat"
-            unelevated
-            class="col-12 col-md-2"
-            color="secondary"
-          >
-            <div>发送(Ctrl+Enter)</div>
-          </q-btn>
-        </div>
-      </div>
-    </div>
-  </q-page>
+    </q-page>
 </template>
 
 <script lang="ts">
@@ -71,225 +71,226 @@ import {createParser, ParsedEvent, ReconnectInterval} from 'eventsource-parser'
 
 
 type Message = {
-  text: string;
-  sent: boolean;
+    text: string;
+    sent: boolean;
 }
 
 type GptMessage = {
-  role: string;
-  content: string
+    role: string;
+    content: string
 }
 
 export default defineComponent({
-  name: 'IndexPage',
-  setup() {
-    let DisplayMessages = ref<Message[]>([])
-    let InputText = ref('')
-    let waitText = ref('')
-    let TotalMessages = ref<GptMessage[]>([])
-    let Loading = ref(false)
-    let Waiting = ref(false)
+    name: 'IndexPage',
+    setup() {
+        let DisplayMessages = ref<Message[]>([])
+        let InputText = ref('')
+        let waitText = ref('')
+        let TotalMessages = ref<GptMessage[]>([])
+        let Loading = ref(false)
+        let Waiting = ref(false)
 
-    const scrollAreaRef = ref()
+        const scrollAreaRef = ref()
 
-    let scrollSize = -1
-    let scrollPercent = 1
-    let scrollPos = 0
-    let bottom = true
+        let scrollSize = -1
+        let scrollPercent = 1
+        let scrollPos = 0
+        let bottom = true
 
-    const meImg = './imgs/me.jpg'
-    const aiImg = './imgs/ai.png'
+        const meImg = './imgs/me.jpg'
+        const aiImg = './imgs/ai.png'
 
-    DisplayMessages.value.push({
-      sent: false,
-      text: "你好，我是OpenAI小助手，基于gpt-3.5-turbo模型，采用ServerLess部署。<br>使用过程中有任何问题可联系：zpzhou.ok@gmail.com"
-    })
-
-    function StreamChat() {
-      if (InputText.value == "") {
-        return
-      }
-
-      DisplayMessages.value.push({
-        sent: true,
-        text: InputText.value
-      })
-
-      TotalMessages.value.push({
-        role: "user",
-        content: InputText.value
-      })
-      InputText.value = ""
-      Loading.value = true
-      waitText.value = ""
-
-      // 流式聊天
-      fetch('/api/streamchat', {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json'
-        },
-        body: JSON.stringify({
-          "model": "gpt-3.5-turbo",
-          "messages": TotalMessages.value
+        DisplayMessages.value.push({
+            sent: false,
+            text: "你好，我是OpenAI小助手，基于gpt-3.5-turbo模型，采用ServerLess部署。<br>使用过程中有任何问题可联系：zpzhou.ok@gmail.com"
         })
-      }).then((response) => {
-        const reader = response.body!.getReader();
-        const decoder = new TextDecoder();
-        const encoder = new TextEncoder()
-        let closed = false
 
-        const stream = new ReadableStream({
-          start(controller) {
-            const streamParser = (event: ParsedEvent | ReconnectInterval) => {
-              if (event.type === 'event') {
-                const data = event.data
-                console.log(data)
-                if (data === '[DONE]') {
-                  controller.close()
-                  closed = true
-                  return
-                }
-
-                if (data !== '[WAIT]') {
-                  try {
-                    const json = JSON.parse(data)
-                    const text = json.choices[0].delta?.content || ''
-                    waitText.value = waitText.value + text
-                    const queue = encoder.encode(text)
-                    controller.enqueue(queue)
-                  } catch (e) {
-                    controller.error(e)
-                  }
-                }
-
-              }
+        function StreamChat() {
+            if (InputText.value == "") {
+                return
             }
 
-            const parser = createParser(streamParser)
-            Loading.value = false
-            Waiting.value = true
+            DisplayMessages.value.push({
+                sent: true,
+                text: InputText.value
+            })
 
-            function parse() {
-              reader.read().then(({value, done}) => {
-                if (done) {
-                  if (!closed) {
-                    controller.close()
-                  }
-                  Waiting.value = false
-                  TotalMessages.value?.push({
-                    role: "assistant",
-                    content: waitText.value
-                  })
+            TotalMessages.value.push({
+                role: "user",
+                content: InputText.value
+            })
+            InputText.value = ""
+            Loading.value = true
+            waitText.value = ""
 
-                  DisplayMessages.value.push({
-                    sent: false,
-                    text: waitText.value
-                  })
-                  return
-                }
-                const text = decoder.decode(value);
-                parser.feed(text);
-                parse()
-              });
-            }
+            // 流式聊天
+            fetch('/api/streamchat', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "model": "gpt-3.5-turbo",
+                    "messages": TotalMessages.value,
+                    "stream": true
+                })
+            }).then((response) => {
+                const reader = response.body!.getReader();
+                const decoder = new TextDecoder();
+                const encoder = new TextEncoder()
+                let closed = false
 
-            parse()
-          }
-        });
-      })
+                const stream = new ReadableStream({
+                    start(controller) {
+                        const streamParser = (event: ParsedEvent | ReconnectInterval) => {
+                            if (event.type === 'event') {
+                                const data = event.data
+                                console.log(data)
+                                if (data === '[DONE]') {
+                                    controller.close()
+                                    closed = true
+                                    return
+                                }
 
-      // add
-    }
+                                if (data !== '[WAIT]') {
+                                    try {
+                                        const json = JSON.parse(data)
+                                        const text = json.choices[0].delta?.content || ''
+                                        waitText.value = waitText.value + text
+                                        const queue = encoder.encode(text)
+                                        controller.enqueue(queue)
+                                    } catch (e) {
+                                        controller.error(e)
+                                    }
+                                }
 
-    function sendMessage() {
-      if (InputText.value == "") {
-        return
-      }
+                            }
+                        }
 
-      DisplayMessages.value.push({
-        sent: true,
-        text: InputText.value
-      })
+                        const parser = createParser(streamParser)
+                        Loading.value = false
+                        Waiting.value = true
 
-      TotalMessages.value.push({
-        role: "user",
-        content: InputText.value
-      })
-      InputText.value = ""
+                        function parse() {
+                            reader.read().then(({value, done}) => {
+                                if (done) {
+                                    if (!closed) {
+                                        controller.close()
+                                    }
+                                    Waiting.value = false
+                                    TotalMessages.value?.push({
+                                        role: "assistant",
+                                        content: waitText.value
+                                    })
 
-      Loading.value = true
-      api.SendMessage({
-        "model": "gpt-3.5-turbo",
-        "messages": TotalMessages.value
-      }).then(response => {
-        Loading.value = false
+                                    DisplayMessages.value.push({
+                                        sent: false,
+                                        text: waitText.value
+                                    })
+                                    return
+                                }
+                                const text = decoder.decode(value);
+                                parser.feed(text);
+                                parse()
+                            });
+                        }
 
-        // 检查出错
-        if (response.data.result.error.type != "") {
-          DisplayMessages.value.push({
-            sent: false,
-            text: "抱歉，OpenAI服务器繁忙，错误：" + response.data.result["error"]["type"]
-          })
-          TotalMessages.value.pop()
+                        parse()
+                    }
+                });
+            })
 
-        } else {
-          let respMessage = response.data.result.choices[0]["message"]["content"]
-          TotalMessages.value?.push({
-            role: "assistant",
-            content: respMessage
-          })
-
-          DisplayMessages.value.push({
-            sent: false,
-            text: respMessage
-          })
+            // add
         }
-      })
-    }
 
-    function scrollToBottom() {
-      scrollAreaRef.value.setScrollPercentage('vertical', 1)
-    }
+        function sendMessage() {
+            if (InputText.value == "") {
+                return
+            }
 
-    function autoScroll() {
-      const scroller = scrollAreaRef.value.getScroll()
-      if (scroller.verticalPosition < scrollPos) {
-        bottom = false
-      }
+            DisplayMessages.value.push({
+                sent: true,
+                text: InputText.value
+            })
 
-      if (scroller.verticalPercentage == 1) {
-        bottom = true
-      }
+            TotalMessages.value.push({
+                role: "user",
+                content: InputText.value
+            })
+            InputText.value = ""
 
-      if (bottom) {
-        scrollToBottom()
-      }
+            Loading.value = true
+            api.SendMessage({
+                "model": "gpt-3.5-turbo",
+                "messages": TotalMessages.value
+            }).then(response => {
+                Loading.value = false
 
-      scrollSize = scroller.verticalSize
-      scrollPos = scroller.verticalPosition
-    }
+                // 检查出错
+                if (response.data.result.error.type != "") {
+                    DisplayMessages.value.push({
+                        sent: false,
+                        text: "抱歉，OpenAI服务器繁忙，错误：" + response.data.result["error"]["type"]
+                    })
+                    TotalMessages.value.pop()
 
-    function handleEnter(e: any) {
-      if (e.ctrlKey) {
-        StreamChat()
-      }
-    }
+                } else {
+                    let respMessage = response.data.result.choices[0]["message"]["content"]
+                    TotalMessages.value?.push({
+                        role: "assistant",
+                        content: respMessage
+                    })
 
-    return {
-      handleEnter,
-      sendMessage,
-      StreamChat,
-      autoScroll,
-      scrollAreaRef,
-      InputText,
-      waitText,
-      DisplayMessages,
-      Loading,
-      Waiting,
-      meImg,
-      aiImg
-    }
-  },
+                    DisplayMessages.value.push({
+                        sent: false,
+                        text: respMessage
+                    })
+                }
+            })
+        }
+
+        function scrollToBottom() {
+            scrollAreaRef.value.setScrollPercentage('vertical', 1)
+        }
+
+        function autoScroll() {
+            const scroller = scrollAreaRef.value.getScroll()
+            if (scroller.verticalPosition < scrollPos) {
+                bottom = false
+            }
+
+            if (scroller.verticalPercentage == 1) {
+                bottom = true
+            }
+
+            if (bottom) {
+                scrollToBottom()
+            }
+
+            scrollSize = scroller.verticalSize
+            scrollPos = scroller.verticalPosition
+        }
+
+        function handleEnter(e: any) {
+            if (e.ctrlKey) {
+                StreamChat()
+            }
+        }
+
+        return {
+            handleEnter,
+            sendMessage,
+            StreamChat,
+            autoScroll,
+            scrollAreaRef,
+            InputText,
+            waitText,
+            DisplayMessages,
+            Loading,
+            Waiting,
+            meImg,
+            aiImg
+        }
+    },
 });
 </script>
