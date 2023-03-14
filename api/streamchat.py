0862ff5ee8d180c041a6af35cf20d1b04186d744
zpzhou <himoker@gmail.com>
2023-03-14 14:03:46 +0800

    update

--- api/streamchat.py
+++ api/streamchat.py
@@ -27,4 +27,3 @@ class Handler(BaseHTTPRequestHandler):
         with requests.post(url, stream=True, headers=headers, data=post_body) as response:
             for chunk in response.iter_content(chunk_size=512):
                 self.wfile.write(chunk)
-                break
