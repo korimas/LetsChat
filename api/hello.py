de80138807f0663b6c4335650f36bb6ff3754fbf
zpzhou <himoker@gmail.com>
2023-03-14 13:37:34 +0800

    update

---
+++ api/hello.py
@@ -0,0 +1,7 @@
+class Handler(BaseHTTPRequestHandler):
+
+    def do_Get(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(b'{"hello": "world"}')
