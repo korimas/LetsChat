b6ca35e41cce6a1b8c98a2291c6e9a45a5b92cb3
zpzhou <himoker@gmail.com>
2023-03-14 16:39:22 +0800

    update

--- api/chat.ts
+++ api/chat.ts
@@ -1,7 +1,9 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 export const config = {
     runtime: 'edge',
 };
 
-export default (req: Request) => {
-    return new Response(`Hello, from ${req.url} I'm now an Edge Function!`);
-};
+export default function (req: VercelRequest, res: VercelResponse) {
+    const { name = 'World' } = req.query;
+    res.send(`Hello ${name}!`);
+}
