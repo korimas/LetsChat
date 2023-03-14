5d7b5895b743baaab2d65a1cdb3e82243d0dcafc
zpzhou <himoker@gmail.com>
2023-03-14 13:51:31 +0800

    update

--- api/hello.py
+++ api/hello.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler
 
 class Handler(BaseHTTPRequestHandler):
 
-    def do_Get(self):
+    def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
