058ac68e09aa98293e12bed98926255455a744c5
zpzhou <himoker@gmail.com>
2023-03-14 16:49:39 +0800

    update

--- api/chat.ts
+++ api/chat.ts
@@ -1,9 +1,43 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { IncomingMessage, ServerResponse } from 'http';
+
 export const config = {
     runtime: 'edge',
 };
 
-export default function (req: VercelRequest, res: VercelResponse) {
-    const { name = 'World' } = req.query;
-    res.send(`Hello ${name}!`);
+
+interface RequestBody {
+    // 在这里定义你需要的请求体字段
+    [key: string]: any;
+}
+
+function parseRequestBody(req: IncomingMessage): Promise<RequestBody> {
+    return new Promise((resolve, reject) => {
+        let body = '';
+        req.on('data', (chunk) => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            try {
+                const json = JSON.parse(body);
+                resolve(json);
+            } catch (error) {
+                reject(error);
+            }
+        });
+        req.on('error', (error) => {
+            reject(error);
+        });
+    });
+}
+
+export default async function (req: IncomingMessage, res: ServerResponse) {
+    try {
+        const body = await parseRequestBody(req);
+        const { name = 'World' } = body;
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`Hello ${name}!`);
+    } catch (error) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end("error");
+    }
 }
